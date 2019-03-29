1adfab6d48d873c82847ba5b7f7c34999184ca5a
AlexPi <alex.pigida@outlook.com>
2019-03-29 17:52:39 -0400

    User preference persistance between sessions.

--- ChsApp/src/app/app.component.ts
+++ ChsApp/src/app/app.component.ts
@@ -15,23 +15,24 @@ export class AppComponent implements OnInit, OnDestroy {
   isLoading: boolean; // used for showing Loading spinner during transition between pages
   isLoggingToServer: boolean;
   private subscription: Subscription;
-  private _themeKey = 'themeKey';
-  private _mainTheme = 'main-theme';
-  private _darkTheme = 'dark-theme';
+  private themeKey = 'themeKey';
+  private mainTheme = 'main-theme';
+  private darkTheme = 'dark-theme';
   themedIcon = './favicon.ico';
   isSignedIn: boolean;
 
   get themeVal(): string {
-    let returnValue = localStorage.getItem(this._themeKey);
+    let returnValue = localStorage.getItem(this.themeKey);
     if (returnValue === null || returnValue.length < 4) {
-      returnValue = this._mainTheme;
+      returnValue = this.darkTheme;
     }
     return returnValue;
   }
   set themeVal(theme: string) {
     if (!(theme === null || theme.length < 4)) {
-      localStorage.setItem(this._themeKey, theme);
+      localStorage.setItem(this.themeKey, theme);
     }
+    console.log(` ** theme just set to : ${localStorage.getItem(this.themeKey)}`);
   }
 
   constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
@@ -59,19 +60,28 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   toggleTheme() {
-    if (this.document.body.classList.contains(this._mainTheme)) {
-      this.document.body.classList.remove(this._mainTheme);
-      this.document.body.classList.add((this.themeVal = this._darkTheme));
+    if (this.document.body.classList.contains(this.mainTheme)) {
+      this.document.body.classList.remove(this.mainTheme);
+      this.document.body.classList.add((this.themeVal = this.darkTheme));
       this.themedIcon = './assets/images/ChsLogo_Dark.png';
     } else {
-      this.document.body.classList.remove(this._darkTheme);
-      this.document.body.classList.add((this.themeVal = this._mainTheme));
+      this.document.body.classList.remove(this.darkTheme);
+      this.document.body.classList.add((this.themeVal = this.mainTheme));
       this.themedIcon = './assets/images/ChsLogo_Lite.png';
     }
+    console.log(` ** theme toggled  to   ${this.themeVal}`);
   }
   setThemeToStoredValue() {
-    if (!this.document.body.classList.contains(this.themeVal)) {
-      this.toggleTheme();
+    console.log(` ** setting to store    ${this.themeVal}`);
+
+    if (this.document.body.classList.contains(this.mainTheme)) {
+      this.document.body.classList.remove(this.mainTheme);
+    } else if (this.document.body.classList.contains(this.darkTheme)) {
+      this.document.body.classList.remove(this.darkTheme);
     }
+
+    this.document.body.classList.add(this.themeVal);
+
+    this.themedIcon = this.themeVal === this.mainTheme ? './assets/images/ChsLogo_Lite.png' : './assets/images/ChsLogo_Dark.png';
   }
 }
