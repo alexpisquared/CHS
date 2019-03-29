84fee6dfef7d32bac89c356d9c56adf637b7851d
AlexPi <alex.pigida@outlook.com>
2019-03-28 20:49:24 -0400

    highlighting of the searched words.

--- ChsApp/src/app/celebrity-list/celebrity-list.component.ts
+++ ChsApp/src/app/celebrity-list/celebrity-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CelebrityDataService } from '../celebrity-data.service';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-celebrity-list',
@@ -10,16 +12,22 @@ import { FormControl } from '@angular/forms';
 export class CelebrityListComponent implements OnInit {
   isSignedIn: boolean;
   searchedWordsControl = new FormControl('');
+  searchedWords$: Observable<string[]> = this.searchedWordsControl.valueChanges.pipe(map((search: string) => search.trim().split(' ')));
   celebs: object;
-  @ViewChild('focus0') usernameField: ElementRef;
+  @ViewChild('focus0') nameField: ElementRef;
+
+  textA0 = new FormControl('A0 - 12345 54321');
+  textA1 = new FormControl('A1 - Ab:cd:ef:gh');
+  textA2 = new FormControl('A2 - 11:22:33:44');
+  textA3 = new FormControl('A3 - 0x79172fabc');
 
   constructor(private data: CelebrityDataService) {}
 
   ngOnInit() {
     this.getCelebs();
     setTimeout(() => {
-      if (this.usernameField !== null) {
-        this.usernameField.nativeElement.focus();
+      if (this.nameField !== null) {
+        this.nameField.nativeElement.focus();
         this.isSignedIn = true;
       }
     }, 333);
@@ -44,5 +52,8 @@ export class CelebrityListComponent implements OnInit {
       this.celebs = data;
       console.log(this.celebs);
     });
+  }
+  addToCart(){
+    console.log(' ** todo: adding to cart ** ');
   }
 }
