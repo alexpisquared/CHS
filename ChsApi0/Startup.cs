635d0ad44c9b33209aa7a43001eb66647adb62b1
AlexPi <alex.pigida@outlook.com>
2019-12-13 18:54:01 -0500

    ++

--- ChsApi0/Startup.cs
+++ ChsApi0/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace ChsApi0
 {
   public class Startup
@@ -26,6 +28,11 @@ namespace ChsApi0
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +48,18 @@ namespace ChsApi0
       app.UseRouting();
 
       app.UseAuthorization();
+
+
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+      // specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
+
 
       app.UseEndpoints(endpoints =>
       {
